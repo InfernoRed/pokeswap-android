b2812a2688ccb00e32af1b455082482b3f614f92
Charlie Castro <ccspart2@gmail.com>
2024-01-25 12:28:28 -0400

    Changes: 1) Addition of the Custom Pokemon Font 2) PlaceHolder Cards for the HomeRoute.kt 3) Further HomeRoute.kt UI implementation

--- app/src/main/java/com/ccspart2/pokeswap_android/presentation/routes/home/HomeRoute.kt
+++ app/src/main/java/com/ccspart2/pokeswap_android/presentation/routes/home/HomeRoute.kt
@@ -1,19 +1,30 @@
 package com.ccspart2.pokeswap_android.presentation.routes.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ccspart2.pokeswap_android.R
 import com.ccspart2.pokeswap_android.presentation.core.ui.PreviewScreen
 import com.ccspart2.pokeswap_android.presentation.routes.home.components.PokedexTopBar
 
@@ -38,12 +49,58 @@ private fun HomeScreen() {
         )
 
         Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(20.dp)
                 .fillMaxSize()
                 .clip(shape = RoundedCornerShape(10.dp))
                 .background(Color.White),
-        ) {}
+        ) {
+            Text(
+                text = "Choose your favorite Pokemon",
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(
+                        top = 35.dp,
+                        start = 25.dp,
+                        end = 25.dp,
+                    ),
+            )
+            Spacer(modifier = Modifier.height(110.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(text = "Aggron")
+                Text(text = "VS")
+                Text(text = "Caterpie")
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.pokemon_sample_card,
+                    ),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(180.dp),
+                )
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.pokemon_sample_card_large,
+                    ),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(180.dp),
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/com/ccspart2/pokeswap_android/ui/theme/Type.kt
+++ app/src/main/java/com/ccspart2/pokeswap_android/ui/theme/Type.kt
@@ -2,70 +2,76 @@ package com.ccspart2.pokeswap_android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ccspart2.pokeswap_android.R
+
+val PokemonFont = FontFamily(
+    Font(R.font.pokemon_solid),
+)
 
 val Typography = Typography(
     displayMedium = TextStyle(
         fontSize = 48.sp,
         lineHeight = 56.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
     displaySmall = TextStyle(
         fontSize = 36.sp,
         lineHeight = 44.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
     headlineLarge = TextStyle(
         fontSize = 34.sp,
         lineHeight = 42.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
         letterSpacing = 0.25.sp,
     ),
     headlineSmall = TextStyle(
         fontSize = 24.sp,
         lineHeight = 32.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
     titleLarge = TextStyle(
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
     titleMedium = TextStyle(
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(500),
     ),
     titleSmall = TextStyle(
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(500),
     ),
     bodyLarge = TextStyle(
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
     bodyMedium = TextStyle(
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
     bodySmall = TextStyle(
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        fontFamily = FontFamily.Default,
+        fontFamily = PokemonFont,
         fontWeight = FontWeight(400),
     ),
 )
